53f359af1e727d388bb61b52d3d40434f108accf
Ryan Batchelder <rb688@nau.edu>
2013-12-24 09:39:03 -0500

    Lots of changed. Asteroids added; working on menu; lose condition exists; multiplier implemented.

--- Spacey Stuff/spaceyStuffMyScene.m
+++ Spacey Stuff/spaceyStuffMyScene.m
@@ -7,10 +7,11 @@
 //
 
 #import "spaceyStuffMyScene.h"
+#include <stdlib.h>
 
 
 @interface spaceyStuffMyScene () <SKPhysicsContactDelegate>
-@property (nonatomic) SKSpriteNode * player;
+//@property (nonatomic) SKSpriteNode * player;
 @property (nonatomic) NSTimeInterval lastSpawnTimeInterval;
 @property (nonatomic) NSTimeInterval lastUpdateTimeInterval;
 @property (nonatomic) SKLabelNode* scoreBoard;
@@ -18,9 +19,21 @@
 
 @implementation spaceyStuffMyScene
 
-static const uint32_t projectileCategory = 0x1 << 0;
-static const uint32_t enemyCategory = 0x1 << 1;
+static const uint32_t playerCategory = 0x1 << 4;
+static const uint32_t projectileCategory = 0x1 << 1;
+static const uint32_t enemyCategory = 0x1 << 2;
+static const uint32_t asteroidCategory = 0x1 << 3;
+static const uint32_t buttonCategory = 0x1 << 3;
+SKSpriteNode *player;
+SKSpriteNode *enemy;
+SKSpriteNode *background1;
+SKSpriteNode *background2;
 int score = 0;
+int multiplier = 1;
+int lives = 3;
+bool playerOnScreen = NO;
+bool boardCleared = NO;
+NSString *scoreString;
 
 -(id)initWithSize:(CGSize)size {    
     if (self = [super initWithSize:size]) {
@@ -29,26 +42,44 @@ int score = 0;
         
         self.backgroundColor = [SKColor colorWithRed:0 green:0 blue:0 alpha:0];
         
-//        SKSpriteNode * bg = [SKSpriteNode spriteNodeWithImageNamed:@"images/background.gif"];
-//        bg.anchorPoint = CGPointZero;
-//        bg.name = @"background";
-//        [self addChild:bg];
+//        SKSpriteNode *background1 = [SKSpriteNode spriteNodeWithImageNamed:@"images/background_static"];
+//        background1.position = CGPointMake(background1.size.width/2, background1.size.height/2);
+//        background1.zPosition = -100;
+//        [self addChild:background1];
+//        SKAction * bgmove = [SKAction moveToX:-background1.size.width/2 duration:3];
+//        SKAction * bgmoveDone = [SKAction removeFromParent];
+//        [background1 runAction:[SKAction sequence:@[bgmove, bgmoveDone]]];
         
-        self.player = [SKSpriteNode spriteNodeWithImageNamed:@"images/SpaceShip"];
-        self.player.position = CGPointMake(50, self.frame.size.height/2);
-        [self addChild:self.player];
+        [self addBackground];
         
+        self.physicsWorld.gravity = CGVectorMake(0, 0);
+        self.physicsWorld.contactDelegate = self;
+        
+    
+        player = [SKSpriteNode spriteNodeWithImageNamed:@"images/SpaceShip"]; //Create player ship TODO: Randomize this!
+        player.position = CGPointMake(50, self.frame.size.height/2); //Set player start position
+        player.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:CGSizeMake(25, 25)]; //Define the PhysicsBody for the player's collision detection
+        player.physicsBody.dynamic = YES;
+        player.physicsBody.categoryBitMask = playerCategory;
+        player.physicsBody.contactTestBitMask = asteroidCategory;
+        player.physicsBody.collisionBitMask = 0;
+        player.physicsBody.usesPreciseCollisionDetection = YES;
+        playerOnScreen = YES;
+        [self addChild:player];
+        
+        NSString *scoreString = [NSString stringWithFormat:@"Score: %d Multiplier: %d Lives: %d", score, multiplier, lives];
         self.scoreBoard = [SKLabelNode labelNodeWithFontNamed:@"Helvetica"];
         
-        self.scoreBoard.text = [NSString stringWithFormat:@"Score: %d", score];
+//        self.scoreBoard.text = [NSString stringWithFormat:@"Score: %d Multiplier: %d", score, multiplier];
+        self.scoreBoard.text = scoreString;
         self.scoreBoard.fontSize = 15;
         self.scoreBoard.fontColor = [SKColor colorWithRed:1 green:1 blue:1 alpha:1];
         self.scoreBoard.position = CGPointMake(self.scene.size.width/2, self.scene.size.height-30);\
         [self addChild:self.scoreBoard];
-
         
-        self.physicsWorld.gravity = CGVectorMake(0, 0);
-        self.physicsWorld.contactDelegate = self;
+        
+
+    
         /* Setup your scene here */
         
 //        self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
@@ -66,8 +97,12 @@ int score = 0;
 }
 
 -(void)addEnemy {
-    SKSpriteNode * enemy = [SKSpriteNode spriteNodeWithImageNamed:@"images/BaddieShip"];
+    int randEnemy = arc4random_uniform(2)+1;
     
+    NSString *enemyType = [NSString stringWithFormat:@"images/BaddieShip%d", randEnemy];
+    
+    SKSpriteNode * enemy = [SKSpriteNode spriteNodeWithImageNamed:enemyType];
+
     CGSize enemySize = CGSizeMake(25, 25);
     
     enemy.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:enemySize];
@@ -75,6 +110,8 @@ int score = 0;
     enemy.physicsBody.categoryBitMask = enemyCategory;
     enemy.physicsBody.contactTestBitMask = projectileCategory;
     enemy.physicsBody.collisionBitMask = 0;
+    
+    enemy.name = [NSString stringWithFormat:@"enemy"];
     
     int minY = enemy.size.height / 2;
     int maxY = self.frame.size.height - enemy.size.height / 2;
@@ -94,12 +131,92 @@ int score = 0;
     [enemy runAction:[SKAction sequence:@[actionMove, actionMoveDone]]];
 }
 
+-(void)addAsteroid {
+    SKSpriteNode * asteroid = [SKSpriteNode spriteNodeWithImageNamed:@"images/asteroid"];
+    
+    //CGSize asteroidSize = CGSizeMake(asteroid.size.width, asteroid.size.height);
+    
+    asteroid.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:25];
+    asteroid.physicsBody.dynamic = YES;
+    asteroid.physicsBody.categoryBitMask = asteroidCategory;
+    asteroid.physicsBody.contactTestBitMask = playerCategory;
+    asteroid.physicsBody.collisionBitMask = 0;
+    
+    asteroid.name = [NSString stringWithFormat:@"asteroid"];
+    
+    int minY = asteroid.size.height / 2;
+    int maxY = self.frame.size.height - asteroid.size.height / 2;
+    int rangeY = maxY - minY;
+    int actualY = (arc4random() % rangeY) + minY;
+    
+    asteroid.position = CGPointMake(self.frame.size.width + asteroid.size.width / 2, actualY);
+    asteroid.anchorPoint = CGPointMake(.4, .5);
+    [self addChild:asteroid];
+    
+//    int minDuration = 1.0;
+//    int maxDuration = 5.0;
+//    int rangeDuration = maxDuration - minDuration;
+//    int actualDuration = (arc4random() % rangeDuration) + minDuration;
+    
+    int actualDuration = arc4random_uniform(5);
+    
+    SKAction * asteroidSpin = [SKAction rotateByAngle:actualDuration duration:0.5];
+    SKAction * actionMove = [SKAction moveTo:CGPointMake(-asteroid.size.width/2, actualY) duration:actualDuration];
+    SKAction * actionMoveDone = [SKAction removeFromParent];
+    [asteroid runAction:[SKAction sequence:@[actionMove, actionMoveDone]]];
+    [asteroid runAction:[SKAction repeatActionForever:asteroidSpin]];
+}
+
+-(void)addBackground {
+    SKSpriteNode * background = [SKSpriteNode spriteNodeWithImageNamed:@"images/background_static"];
+    
+    background.position = CGPointMake(self.size.width + background.size.width/2, self.size.height/2);
+    background.zPosition = -100;
+    [self addChild:background];
+    SKAction * bgmove = [SKAction moveToX:-background.size.width/2 duration:3];
+    SKAction * bgmoveDone = [SKAction removeFromParent];
+    [background runAction:[SKAction sequence:@[bgmove, bgmoveDone]]];
+}
+
 -(void)updateWithtimeSinceLastUpdate:(CFTimeInterval)timeSinceLast {
     self.lastSpawnTimeInterval += timeSinceLast;
-    if (self.lastSpawnTimeInterval > 1) {
+    if (self.lastSpawnTimeInterval > 1 && lives > 0) {
         self.lastSpawnTimeInterval = 0;
         [self addEnemy];
+        [self addAsteroid];
+        [self addBackground];
+        if (!playerOnScreen) {
+            [self addChild:player];
+            playerOnScreen = YES;
+        }
+        if (score % 100 == 0) {
+            lives++;
+        }
     }
+    else if (lives == 0 && !boardCleared) { //Game is over and we still need to clear the board
+        [self addBackground];
+        boardCleared = [self doClearBoard];
+    }
+    
+    else if (lives == 0 && boardCleared && self.lastSpawnTimeInterval > 1) { //Game is over and the board is already cleared
+        self.lastSpawnTimeInterval = 0;
+        [self addBackground];
+        if (!playerOnScreen) {
+            [self addChild:player];
+            playerOnScreen = YES;
+        }
+    }
+}
+
+-(BOOL)doClearBoard {
+    self.scoreBoard.text = [NSString stringWithFormat:@"Game Over!"];
+    [self enumerateChildNodesWithName:[NSString stringWithFormat:@"asteroid"] usingBlock:^(SKNode *node, BOOL *stop) {
+        [node removeFromParent];
+    }];
+    [self enumerateChildNodesWithName:[NSString stringWithFormat:@"enemy"] usingBlock:^(SKNode *node, BOOL *stop) {
+        [node removeFromParent];
+    }];
+    return YES;
 }
 
 -(void)update:(NSTimeInterval)currentTime {
@@ -118,7 +235,7 @@ int score = 0;
 //    CGPoint previousLocation = [touch previousLocationInNode:self];
 //    
 //    CGPoint translation = CGPointMake(100, location.y - previousLocation.y);
-//    self.player.position = translation;
+//    player.position = translation;
 //                    
 //}
 
@@ -146,31 +263,44 @@ int score = 0;
     //CGPoint location = [touch locationInNode:self];
     
     SKSpriteNode * projectile = [SKSpriteNode spriteNodeWithImageNamed:@"images/PewPew"];
-    projectile.position = CGPointMake(self.player.position.x+5, self.player.position.y);
+    projectile.position = CGPointMake(player.position.x+5, player.position.y);
     CGSize projectileSize = CGSizeMake(15, 25);
     projectile.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:projectileSize];
     projectile.physicsBody.dynamic = YES;
     projectile.physicsBody.categoryBitMask = projectileCategory;
     projectile.physicsBody.contactTestBitMask = enemyCategory;
-    projectile.physicsBody.contactTestBitMask = 0;
+    projectile.physicsBody.collisionBitMask = 0;
     projectile.physicsBody.usesPreciseCollisionDetection = YES;
     
     [self addChild:projectile];
     
-    float velocity = 480.0/1.0;
+    float velocity = 480.0;
     float realMoveDuration = self.size.width / velocity;
-    CGPoint dest = CGPointMake(self.frame.size.width+10, self.player.position.y);
+    CGPoint dest = CGPointMake(self.frame.size.width+10, player.position.y);
     SKAction * actionMove = [SKAction moveTo:dest duration:realMoveDuration];
     SKAction * actionMoveDone = [SKAction removeFromParent];
     [projectile runAction:[SKAction sequence:@[actionMove, actionMoveDone]]];
 }
 
 -(void)projectile:(SKSpriteNode *)projectile didCollideWithEnemy:(SKSpriteNode *)enemy {
-    NSLog(@"Hit");
-    score++;
-    self.scoreBoard.text = [NSString stringWithFormat:@"Score: %d", score];
+    //NSLog(@"Hit"); Used for debugging
+    score += multiplier;
+    if (score % 5 == 0) {
+        multiplier++;
+    }
+    self.scoreBoard.text = [NSString stringWithFormat:@"Score: %d Multiplier: %d Lives: %d", score, multiplier, lives];
     [projectile removeFromParent];
     [enemy removeFromParent];
+}
+
+-(void)asteroid:(SKSpriteNode *)asteroid didCollideWithPlayer:(SKSpriteNode *)player {
+    NSLog(@"Asteroid hit");
+    lives--;
+    multiplier = 1;
+    self.scoreBoard.text = [NSString stringWithFormat:@"Score: %d Multiplier: %d Lives: %d", score, multiplier, lives];
+    [asteroid removeFromParent];
+    [player removeFromParent];
+    playerOnScreen = NO;
 }
 
 -(void)didBeginContact:(SKPhysicsContact *)contact {
@@ -188,17 +318,20 @@ int score = 0;
     if ((firstBody.categoryBitMask & projectileCategory) != 0 && (secondBody.categoryBitMask & enemyCategory) != 0) {
         [self projectile:(SKSpriteNode *) firstBody.node didCollideWithEnemy:(SKSpriteNode *) secondBody.node];
     }
+    if ((firstBody.categoryBitMask & asteroidCategory) != 0 && (secondBody.categoryBitMask & playerCategory) != 0) {
+        [self asteroid:(SKSpriteNode *) firstBody.node didCollideWithPlayer:(SKSpriteNode *) secondBody.node];
+    }
 }
 
 -(void)handlePanFrom:(UIPanGestureRecognizer *)recognizer {
     if (recognizer.state == UIGestureRecognizerStateChanged) {
         CGPoint translation = [recognizer translationInView:recognizer.view];
         translation = CGPointMake(100, -translation.y);
-        [self.player setPosition:CGPointMake(self.player.position.x, self.player.position.y + translation.y)];
-        if (self.player.position.y > self.scene.size.height + 50)
-            [self.player setPosition:CGPointMake(self.player.position.x, -50)];
-        else if (self.player.position.y < -50)
-            [self.player setPosition:CGPointMake(self.player.position.x, self.scene.size.height+50)];
+        [player setPosition:CGPointMake(player.position.x, player.position.y + translation.y)];
+        if (player.position.y > self.scene.size.height + 50)
+            [player setPosition:CGPointMake(player.position.x, -50)];
+        else if (player.position.y < -50)
+            [player setPosition:CGPointMake(player.position.x, self.scene.size.height+50)];
         [recognizer setTranslation:CGPointZero inView:recognizer.view];
     }
     
